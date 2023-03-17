ef155a4c4cd8649b2dea220372cb1a8a29dd5a0b
Juan Jimenez <juanjiimenez8@gmail.com>
2023-03-17 17:38:23 -0500

    version final

--- src/components/HomeComponent/HomeComponent.jsx
+++ src/components/HomeComponent/HomeComponent.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Card, CardBody, CardHeader } from 'reactstrap';
 
 export const HomeComponent = () => {
     return (
-        <div>
-            <h1>HOME</h1>
+        <div className='container'>
+            <h1 className='text-center'>HOME</h1>
+            <div className='row'>
+                <div className='col mb-5' style={{textAlign:'-webkit-center'}}>
+                    <Card style={{width:'25rem'}}>
+                        <CardHeader className='bg-dark text-light'>
+                            Rubria Corte 1 - Desarrollo Web 2
+                        </CardHeader>
+                        <CardBody>
+                            <p>Juan Jimenez Yancy</p>
+                        </CardBody>
+                    </Card>
+                </div>
+            </div>
+
         </div>
     )
 }
