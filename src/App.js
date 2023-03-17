f4e5c6e17df647f249f748eb0f5ec9fa695cc9e2
Juan Jimenez <juanjiimenez8@gmail.com>
2023-03-17 18:12:58 -0500

    router@6

--- src/App.js
+++ src/App.js
@@ -7,20 +7,21 @@ import { FooterComponent } from './components/FooterComponent/FooterComponent';
 import { HeaderComponent } from './components/HeaderComponent/HeaderComponent';
 import { HomeComponent } from './components/HomeComponent/HomeComponent'
 
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateComponent from './components/CreateComponent/CreateComponent';
 
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <Routes>
-        <Route path={'/'} element={<><HomeComponent /></>}></Route>
-        <Route path={'/content'} element={<><DetalleComponent /></>}></Route>
-        <Route path={'/create'} element={<><CreateComponent /></>}></Route>
-      </Routes>
-
-      <FooterComponent className="my-3" />
+      <Router>
+        <HeaderComponent/>
+        <Routes>
+          <Route path={'/'} element={<><HomeComponent/></>}></Route>
+          <Route path={'/content'} element={<><DetalleComponent/></>}></Route>
+          <Route path={'/create'} element={<><CreateComponent/></>}></Route>
+        </Routes>
+        <FooterComponent/>
+      </Router>
     </>
   );
 }
